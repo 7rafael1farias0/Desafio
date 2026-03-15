11220cc8482d289ff841ad796cc1adc431fa2d85
7rafael1farias0 <rafaelfarias.borges@gmail.com>
2026-03-15 06:02:30 -0300

    update

--- mediaEscolar.js
+++ mediaEscolar.js
@@ -1,14 +1,3 @@
-// Fazer um programa que receba a média de um aluno.
-// Caso a média seja < 5 imprima "Reprovado"
-// Caso a média seja >= 5 e < 7 imprima "Recuperação"
-// Caso a média seja >= 7 imprima "Aprovado"
-
-//     Exemplo:
-//         Entrada:
-//             5.5
-//         Saída:
-//             Recuperação
-
 
 const { gets, print } = require('./funcoes/funcoes-auxiliares1');
 const media = gets();
